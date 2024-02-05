f3d0df1ca00dda8123fce6597abc94e524756c96
Emily Ochoa <eochoa156@gmail.com>
2024-02-05 15:48:39 -0800

    redid GET

--- public/index.js
+++ public/index.js
@@ -1,26 +1,16 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks',{
-        method: "GET",
+    let response = await fetch('http://localhost:3001/listBooks', {
+        method: 'GET',
         headers: null,
-        body: null,
+        body: null
     });
-    let books = await response.json()
+
+    let books = await response.json();
     console.log(books);
 
     books.forEach(renderBook)
 }
-
-let response = await fetch('http://localhost:9001/updateBook', {
-    method: "PATCH",
-    headers: {'Content-Type' : 'application/json'},
-    body: JSON.stringify({
-        "id": 3,
-        "title": "Legends of Arathrae",
-    })
-})
-let updatedBook = await response.json()
-console.log(updatedBook);
 
 function renderBook(book) {
     let bookContainer = document.querySelector('.book-container')
