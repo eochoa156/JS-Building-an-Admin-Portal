c00fa3abda0389cb81e3f6ed06b001757d7df264
Emily Ochoa <eochoa156@gmail.com>
2024-02-05 10:13:13 -0800

    changed admin js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,11 @@
+let adminResponse = await fetch('http://localhost:3000/admin.html',{
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({}),
+});
+let books = await response.json()
+console.log(books);
 
-// Your Code Here
+books.forEach(renderBook)
