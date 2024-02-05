9a3b4051ce92f2576ff8ba8a3e24413a304539a3
Emily Ochoa <eochoa156@gmail.com>
2024-02-05 10:01:38 -0800

    updated book

--- public/index.js
+++ public/index.js
@@ -1,10 +1,26 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
+    let response = await fetch('http://localhost:3001/listBooks',{
+        method: "GET",
+        headers: null,
+        body: null,
+    });
     let books = await response.json()
+    console.log(books);
 
     books.forEach(renderBook)
 }
+
+let response = await fetch('http://localhost:9001/updateBook', {
+    method: "PATCH",
+    headers: {'Content-Type' : 'application/json'},
+    body: JSON.stringify({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    })
+})
+let updatedBook = await response.json()
+console.log(updatedBook);
 
 function renderBook(book) {
     let bookContainer = document.querySelector('.book-container')
