d0f5fe0d1073ac80300041e8b2fdf2846e9627fd
Emily Ochoa <eochoa156@gmail.com>
2024-02-05 16:45:40 -0800

    moved coding to admin file

--- public/admin.js
+++ public/admin.js
@@ -1,11 +1,43 @@
-let adminResponse = await fetch('http://localhost:3000/admin.html',{
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({}),
+let response = await fetch('http://localhost:3001/listBooks', {
+    method: 'GET',
+    headers: null,
+    body: null
 });
-let books = await response.json()
+
+let books = await response.json();
 console.log(books);
 
 books.forEach(renderBook)
+
+let updateBook = await fetch('http://localhost:9001/updateBook', {
+    method: 'PATCH',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    }),
+})
+
+let updatedBook = await updateBook.json();
+console.log(updatedBook);
+
+function renderBook(book) {
+    let bookContainer = document.querySelector('.book-container')
+    bookContainer.innerHTML += `
+        <div class="col-sm-3">
+            <div class="card" style="width: 100%;">
+                ${book.imageURL ? `
+                    <img class="card-img-top" src="${book.imageURL}" />
+                `
+                : ``}
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
+                    <p class="card-text">${book.description}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
--- public/index.js
+++ public/index.js
@@ -1,29 +1,9 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks', {
-        method: 'GET',
-        headers: null,
-        body: null
-    });
-
-    let books = await response.json();
-    console.log(books);
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
 
     books.forEach(renderBook)
-
-    let updateBook = await fetch('http://localhost:9001/updateBook', {
-        method: 'PATCH',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify({
-            "id": 3,
-            "title": "Legends of Arathrae",
-        }),
-    })
-
-    let updatedBook = await updateBook.json();
-    console.log(updatedBook);
 }
 
 function renderBook(book) {
